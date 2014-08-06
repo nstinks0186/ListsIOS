206b01e3777b6d58922a31fe1b78a4afaa44cffe
Pinuno Fuentes <nstinks0186@gmail.com>
2014-08-06 14:33:41 -0400

    Initial Commit

---
+++ ListsIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ListsIOS
+//
+//  Created by Pinuno on 8/6/14.
+//  Copyright (c) 2014 UNO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
